2fedc7794803bc6230c59b266abed913ad6be679
senti-kike <takasakisen.js24.ju07@gmail.com>
2022-04-07 21:46:22 +0900

    test

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('../vendor/autoload.php');
-require("public/home.php"); 
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-?>
---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php
+require('../vendor/autoload.php');
+require("home.php"); 
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+?>
